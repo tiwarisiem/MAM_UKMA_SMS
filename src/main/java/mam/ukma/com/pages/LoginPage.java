af8eade0f1617edca24ade30a71c5bc6957ca998
Anupam Kumar Tiwari <tiwarisiem@gmail.com>
2016-09-17 00:33:04 +0530

    September 17 mid night

--- src/main/java/mam/ukma/com/pages/LoginPage.java
+++ src/main/java/mam/ukma/com/pages/LoginPage.java
@@ -1,7 +1,5 @@
 package mam.ukma.com.pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
--- src/test/java/mam/ukma/com/testcases/LoginTest.java
+++ src/test/java/mam/ukma/com/testcases/LoginTest.java
@@ -7,11 +7,20 @@ import org.testng.annotations.Test;
 
 import mam.ukma.com.pages.LaunchPage;
 import mam.ukma.com.pages.LoginPage;
+import mam.ukma.com.util.TestUtil;
+import mam.ukma.com.util.Xls_Reader;
 
 public class LoginTest extends BaseTest{
 	
+	//Xls_Reader xls=new Xls_Reader(System.getProperty("user.dir"+"\\xls\\Authentication.xlsx"));
+	Xls_Reader xls=new Xls_Reader("F:\\Selenium_Maven\\Project_Directory\\MAM_UKMA_SMS\\xls\\Authentication.xlsx");
 	@Test(dataProvider="getData")
-	public void invalidLogin(String username, String password){
+	public void invalidLoginTest(
+					String runMode,
+					String browser,
+					String username, 
+					String password,
+					String expectedResult){
 		LaunchPage lp=launchBrowser("chrome");
 		LoginPage loginPage=lp.goToHomePage("DEV");
 		loginPage.doLogin(username, password);
@@ -19,16 +28,10 @@ public class LoginTest extends BaseTest{
 			
 	}
 	
+	
 	@DataProvider
 	public Object[][] getData(){
-		
-		String[][] arr=new String[2][2];
-		arr[0][0]="Admin";
-		arr[0][1]="Globrin@313";
-		arr[1][0]="Adminyy";
-		arr[1][1]="Globrin@313";
-
-		return arr;
+		return TestUtil.getData(xls, "LoginTest");
 	}
 
 }
